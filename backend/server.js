ce8e47b032b36e65f0690b79017a96f64d181e50
Maxim <sudorgin.m@inbox.ru>
2023-10-20 11:55:23 +0600

    AAA6

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,6 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 4000;
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
 });
-
